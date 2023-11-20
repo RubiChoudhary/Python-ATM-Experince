da5250addb0ddf588b1b03863b025ee84d7d711c
Rubi Choudhary <94670586+RubiChoudhary@users.noreply.github.com>
2023-11-20 20:48:04 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,68 @@
+class ATM:
+    def __init__(self):     # Constructor
+        self.pin = ""
+        self.balance = 0
+        self.menu()
+
+    def menu(self):
+        user_input = input("""
+        Hello, How would you like to proceed?
+        1. Enter 1 to create the pin
+        2. Enter 2 to deposit
+        3. Enter 3 to withdraw
+        4. Enter 4 to check Balance
+        5. Enter 5 to Exit.
+        """)
+
+        if user_input == "1":
+            self.create_pin()
+        elif user_input == "2":
+            self.deposit()
+        elif user_input == "3":
+            self.withdraw()
+        elif user_input == "4":
+            self.check_balance()
+        else:
+            print("Exit")
+
+    def create_pin(self):
+        self.pin = input("Enter your pin")
+        print("Pin set successfully")
+
+        self.menu()
+
+
+    def deposit(self):
+        temp = input("Enter your pin")
+        if temp == self.pin:
+            amount = int(input("Enter the amount"))
+            self.balance = self.balance + amount
+            print("Deposit Successfully")
+        else:
+            print("Invalid Pin")
+
+            self.menu()
+
+    def withdraw(self):
+        temp = input("Enter the pin")
+        if temp == self.pin:
+            amount = int(input("Enter the amount"))
+            if amount < self.balance:
+                self.balance = self.balance - amount
+                print("Operation Successfull")
+            else:
+                print("insufficient funds")
+        else:
+            print("Invalid pin")
+
+            self.menu()
+
+    def check_balance(self):
+        temp = input("Enter the pin")
+        if temp == self.pin:
+            print(self.balance)
+        else:
+            print("Invalid Pin")
+
+            self.menu()
+
